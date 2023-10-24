a29c2c11278f47c0cc6b977f8e1f6438b76ac2d2
swyth_ <swyth+github@pm.me>
2023-10-24 21:21:29 +0200

    test axesHelper for position object in space

--- 05-Transform-objects/src/script.js
+++ 05-Transform-objects/src/script.js
@@ -12,11 +12,14 @@ const scene = new THREE.Scene()
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
-
-scene.add(mesh)
 // Another manner to set position of an object
 mesh.position.set(0.7, -0.6, 1)
+scene.add(mesh)
 
+
+// Axes Helper
+const axesHelper = new THREE.AxesHelper()
+scene.add(axesHelper)
 /**
  * Sizes
  */

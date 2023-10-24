e5c4a1254d0696222463661229ba78df8811668c
swyth_ <swyth+github@pm.me>
2023-10-24 21:19:23 +0200

    test set() method to place object

--- 05-Transform-objects/src/script.js
+++ 05-Transform-objects/src/script.js
@@ -12,14 +12,10 @@ const scene = new THREE.Scene()
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
 const mesh = new THREE.Mesh(geometry, material)
-// Moving the object before to render, and after creating the mesh
-mesh.position.x = 0.7 // Going to the right
-mesh.position.y = -0.6 // Going to the down
-mesh.position.z = 1 // Going to the back
+
 scene.add(mesh)
-// normalize() will take the vector length and normalize to 1.
-mesh.position.normalize()
-console.log(mesh.position.length()) // Distance between center of the scene and center of object
+// Another manner to set position of an object
+mesh.position.set(0.7, -0.6, 1)
 
 /**
  * Sizes
